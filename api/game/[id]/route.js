8026b558810f2bee7b62e11c55e190dea6a70174
Sujit "Kevin" Dityam <kevindityam@gmail.com>
2024-09-28 17:25:17 +0700

    Delete api directory

--- api/game/[id]/route.js
+++
@@ -1,7 +0,0 @@
-import gameS from "@/models/gameM";
-
-export async function GET(request, { params }) {
-    const id = params.id;
-    const category = await gameS.findById(id)
-    return Response.json(category);
-}
